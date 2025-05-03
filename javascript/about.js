fe430e8d1ed0b7e429d8291f4b6570c7379a27cc
Your Name <you@example.com>
2025-05-03 17:25:25 +0530

    adout page counter add

--- javascript/about.js
+++ javascript/about.js
@@ -1,5 +1,13 @@
 import Footer from "../components/footer.js";
 import Navbar from "../components/navbar.js";
+import { counter } from "../utils/counter.js";
 
-document.getElementById("navbar").innerHTML=Navbar()
-document.getElementById("footer").innerHTML=Footer()
+document.getElementById("navbar").innerHTML = Navbar();
+document.getElementById("footer").innerHTML = Footer();
+
+window.onload = () => {
+  counter("value1");
+  counter("value2");
+  counter("value3");
+  counter("value4");
+};
