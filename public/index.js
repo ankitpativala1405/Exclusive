22c3e352b93bf143df3d59fa579c6053d512c055
Your Name <you@example.com>
2025-05-06 13:46:14 +0530

    update

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-const DbConnect = require("./config/dbconnect");
+const DbConnect = require("../config/dbconnect");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -6,7 +6,7 @@ const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 const PORT = process.env.PORT || 4000;
-const router = require("./routes/userrouter");
+const router = require("../routes/userrouter");
 
 app.use("/user", router);
 
