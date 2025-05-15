658bf5a6d9dfff1ef163311d5049b1ba5c3d3c1b
ankitpativala1405 <pativalaankit@gmail.com>
2025-05-15 15:55:53 +0530

    sign up validation complete

--- javascript/signup.js
+++ javascript/signup.js
@@ -47,18 +47,65 @@ document.getElementById("signupform").addEventListener("submit", async (e) => {
     username,
   };
 
+  const NameREGEX = /^[A-Z][a-z]+(?: [A-Z][a-z]+)*$/;
+  const EmailREGEX = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  const NumberREGEX = /^(?:\+91[\-\s]?|0)?[6-9]\d{9}$/;
+  let easypasswordregex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{5,8}$/;
+  let mediumPasswordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"'<>,.?/-]).{9,12}$/;
+  let strongpasswordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*()_+={}\[\]:;"'<>,.?/-]).{13,}$/;
+
+  if (!NameREGEX.test(name)) {
+    alert(`Please Enter Valid Name... Server Want \nFirst Later is Capital`);
+    return;
+  }
+  if (!EmailREGEX.test(email)) {
+    alert(`Please Enter Valid Email...`);
+    return;
+  }
+  if (!NumberREGEX.test(number)) {
+    alert(
+      `Please enter a valid Indian mobile number. It can start with +91, 0, or just the 10-digit number beginning with 6-9.`
+    );
+    return;
+  }
+
+  if (
+    !easypasswordregex.test(password) &&
+    !mediumPasswordRegex.test(password) &&
+    !strongpasswordRegex.test(password)
+  ) {
+    alert(
+      "Your password should be at least 5 characters long, contain at least one uppercase letter, one lowercase letter, one number, and one special character."
+    );
+    return;
+  } else {
+    if (easypasswordregex.test(password)) {
+      alert("Your password is easy to guess");
+    } else if (mediumPasswordRegex.test(password)) {
+      alert("Your password is medium strength");
+    } else if (strongpasswordRegex.test(password)) {
+      alert("Your password is strong");
+    }
+  }
+
   let AllUser = await UserMethod.GetAll();
   let IsExist = AllUser.find((users) => users.number == user.number);
   let IsExistEmail = AllUser.find((users) => users.email == user.email);
   if (IsExist && IsExistEmail) {
-    alert("Your Number And Email Has Already Taken Please please Try with Another Number And Email ...");
+    alert(
+      "Your Number And Email Has Already Taken Please please Try with Another Number And Email ..."
+    );
     return;
   }
   if (IsExist) {
-    alert("Your Number Has Already Taken Please please Try with Another Number...");
+    alert(
+      "Your Number Has Already Taken Please please Try with Another Number..."
+    );
     return;
   }
-   if (IsExistEmail) {
+  if (IsExistEmail) {
     alert(
       "Your Email Has Already Taken Please please Try with Another Email..."
     );
