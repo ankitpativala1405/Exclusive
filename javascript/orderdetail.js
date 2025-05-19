6528f6ce3c11cf31a55b259e84436fa2e1c12fcc
Your Name <you@example.com>
2025-05-19 21:25:19 +0530

    update

--- javascript/orderdetail.js
+++ javascript/orderdetail.js
@@ -1,7 +1,16 @@
 import CompanyPolicy from "../components/companypolicy.js";
 import Footer from "../components/footer.js";
 import Navbar from "../components/navbar.js";
+import { ExportCartCount } from "./cart.js";
 
-document.getElementById("navbar").innerHTML=Navbar()
-document.getElementById("companypolicy").innerHTML=CompanyPolicy()
-document.getElementById("footer").innerHTML=Footer()
+document.getElementById("navbar").innerHTML = Navbar();
+document.getElementById("companypolicy").innerHTML = CompanyPolicy();
+document.getElementById("footer").innerHTML = Footer();
+
+document.addEventListener("DOMContentLoaded", async () => {
+
+  const count = await ExportCartCount();
+  document.getElementById("cart-count").innerText = `( ${count} )`;
+
+  
+});
